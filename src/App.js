ac579f337d0bdbb2d64d646e938e7525708f8992
kishore 01 <vkkishore886@gmail.com>
2024-12-06 12:30:53 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';  
 import 'bootstrap/dist/css/bootstrap.min.css';
 import log from "./Images/logo-light.png"
-import { IoLocationOutline } from "react-icons/io5";
-
-
-
-
-
+import { GrLocation } from "react-icons/gr";
+import { GoSearch } from "react-icons/go";
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
-import { FaSearch, FaMapMarkerAlt, FaUserCircle } from 'react-icons/fa'; 
 
 function CustomNavbar() {
   return (
@@ -19,16 +14,15 @@ function CustomNavbar() {
       <Container fluid className="p-0">
         <div className="d-flex align-items-center p-0 ">
           <Navbar.Toggle aria-controls="responsive-navbar-nav flex-row" className="d-lg-none float-start p-0" />
-
-          <Navbar.Brand className='p-0 position-relative' href="#home">
+          <Navbar.Brand className='px-1 mb-1 position-relative' href="#home">
               <img
               className='forlogo position-absoulte top-0 p-0'
                 src={log} 
                 alt="Logo"
               />
             <Button variant="link" className=" location_botton allign-item-center px-3 ">
-            <IoLocationOutline size={25} style={{ float: "left" }} />
-              <p className='text-decoration-none'>Event in coimbatore</p>
+            <GrLocation  size={24} style={{  float: "left", color:"white",strokeWidth: "5"  }} />
+              <p className='location_anchour mx-3'>Event in coimbatore</p>
             </Button>
           </Navbar.Brand>
         </div>
@@ -36,25 +30,40 @@ function CustomNavbar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto d-flex align-items-center">
             <Nav.Link className="d-none d-md-block">
-              <Button variant="outline-primary">
-                <FaSearch />
+              <Button style={{ width: "129px", borderColor: "white" }} className='pointer-events-none location_bottonRR' variant="outline-primary">
+                <p className='location_anchour'>List your event</p>
+              </Button>
+              <Button style={{width:"125px"}} className='pointer-events-none location_bottonRR mx-2' variant="outline-primary">
+                <p className='location_anchour'>Work with us</p>
               </Button>
             </Nav.Link>
 
-            <Nav.Link>
-              <Button variant="outline-primary" className="rounded-circle" style={{ width: '40px', height: '40px' }}>
-                <FaUserCircle />
-              </Button>
-            </Nav.Link>
+
+            <Button
+            variant="outline-primary"
+            className="pointer-events-none rounded-circle border-white d-flex justify-content-center align-items-center"
+            style={{ width: '40px', height: '40px' }}
+            >
+            <GoSearch size={70} style={{ color: "white" }} />
+           </Button>
           </Nav>
         </Navbar.Collapse>
 
         <Nav className="ms-auto">
-          <Nav.Link className="d-none d-md-block">
-            <Button variant="outline-primary">
-              <FaMapMarkerAlt />
+          <Nav.Link className="d-none d-md-block loca ">
+            <Button variant="outline-primary" className='pointer-events-none location_bottonR'>
+            <GrLocation  size={21} style={{  float: "left", color:"white",strokeWidth: "2"  }} />
+            <p className='location_anchour'style={{fontSize:"15px"}}>Coimbatore</p>
             </Button>
           </Nav.Link>
+          <Nav.Link>
+          <Button
+            variant="outline-primary"
+            className="pointer-events-none dummy_UM rounded-circle border-white d-flex flex-direction-row justify-content-center align-items-center"
+            style={{ width: '40px', height: '40px' }}>
+              <p className='location_anchour mt-3'style={{fontSize:"15px"}}>UM</p>
+           </Button>
+           </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
@@ -74,7 +83,7 @@ function CustomNavbar() {
 
 function App() { 
   return ( 
-    <Container fluid className="there_you_go"> 
+    <Container fluid className="there_you_go px-2 py-1 px-1"> 
       <CustomNavbar /> 
     </Container> 
   ); 
