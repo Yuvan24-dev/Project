366b8e580963ca85120a58fca9203b060f6f7a16
kishore 01 <vkkishore886@gmail.com>
2024-12-09 11:22:18 +0530

    Courousal initiated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css'; 
-import Row from 'react-bootstrap/Row'; 
-import Col from 'react-bootstrap/Col';  
 import 'bootstrap/dist/css/bootstrap.min.css';
 import log from "./Images/logo-light.png"
 import { GrLocation } from "react-icons/gr";
 import { GoSearch } from "react-icons/go";
 import React from 'react';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button,Carousel,Row,Col } from 'react-bootstrap';
 
 function CustomNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" variant="light" sticky="top" className="w-100 there_you_go p-0">
+    <Navbar collapseOnSelect expand="lg" variant="light" sticky="top" className="w-100 there_you_go pt-0 pb-3">
       <Container fluid className="p-0">
         <div className="d-flex align-items-center p-0 ">
           <Navbar.Toggle aria-controls="responsive-navbar-nav flex-row" className="d-lg-none float-start p-0" />
@@ -56,19 +54,128 @@ function CustomNavbar() {
             <p className='location_anchour'style={{fontSize:"15px"}}>Coimbatore</p>
             </Button>
           </Nav.Link>
-          <Nav.Link>
-          <Button
-            variant="outline-primary"
-            className="pointer-events-none dummy_UM rounded-circle border-white d-flex flex-direction-row justify-content-center align-items-center"
-            style={{ width: '40px', height: '40px' }}>
-              <p className='location_anchour mt-3'style={{fontSize:"15px"}}>UM</p>
-           </Button>
-           </Nav.Link>
+          <Nav.Link className="visible-only-on-large">
+  <Button
+    variant="outline-primary"
+    className="rounded-circle border-white d-flex justify-content-center align-items-center"
+    style={{ width: '40px', height: '40px' }}>
+    <p className='location_anchour mt-3' style={{ fontSize: "15px" }}>UM</p>
+  </Button>
+</Nav.Link>
+
+
+
         </Nav>
       </Container>
     </Navbar>
   );
 }
+
+
+
+const CustomCarousel = () => {
+  return (
+    <Carousel>
+      {/* Slide 1 */}
+      <Carousel.Item className='py-5'>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="First slide - Image 1"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="First slide - Image 2"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="First slide - Image 3"
+            />
+          </Col>
+        </Row>
+        <Carousel.Caption>
+          <h3>Vijay Antony 3.0 Live Concert</h3>
+          <button className="btn btn-warning">BOOK NOW</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* Slide 2 */}
+      <Carousel.Item className='py-5'>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Second slide - Image 1"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Second slide - Image 2"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Second slide - Image 3"
+            />
+          </Col>
+        </Row>
+        <Carousel.Caption>
+          <h3>22nd Chennai International Film Festival</h3>
+          <button className="btn btn-warning">BOOK NOW</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* Slide 3 */}
+      <Carousel.Item className='py-5'>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Third slide - Image 1"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Third slide - Image 2"
+            />
+          </Col>
+          <Col md={4}>
+            <img
+              className="img-fluid"
+              src={log}
+              alt="Third slide - Image 3"
+            />
+          </Col>
+        </Row>
+        <Carousel.Caption>
+          <h3>Movies, Dining, Events</h3>
+          <button className="btn btn-warning">Discover Now</button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+
+
+
+
 
 
 
@@ -85,6 +192,7 @@ function App() {
   return ( 
     <Container fluid className="there_you_go px-2 py-1 px-1"> 
       <CustomNavbar /> 
+      <CustomCarousel />
     </Container> 
   ); 
 } 
